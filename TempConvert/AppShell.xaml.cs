0dcbbbd41082ea690c05151dc8c245ce7fb829bd
Bill Chin <chinww@vcu.edu>
2024-04-10 18:21:03 -0400

    Add project files.

---
+++ TempConvert/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TempConvert
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TempConvert/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace TempConvert
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertButton_Clicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(CelsiusEntry.Text, out double celsius))
+            {
+                double fahrenheit = UnitConverters.CelsiusToFahrenheit(celsius);
+                FahrenheitEntry.Text = $"{fahrenheit:F2}";
+            }
+            else
+            {
+                // Handle invalid input (e.g., non-numeric values).
+                ErrorTextField.Text = "Invalid input. Please enter a valid Celsius temperature.";
+            }
+        }
+
+        private void SwitchDirectionClicked(object sender, EventArgs e)
+        {
+            count++;
+            ErrorTextField.Text = $"Switch direction button pressed {count} times";
+        }
+    }
+
+}
