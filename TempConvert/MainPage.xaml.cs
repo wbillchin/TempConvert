56232749b16898f29c82383a674e638ef19973b5
Bill Chin <chinww@vcu.edu>
2024-04-10 21:34:49 -0400

    Implemented switching direction of conversion.

--- TempConvert/MainPage.xaml.cs
+++ TempConvert/MainPage.xaml.cs
@@ -3,7 +3,7 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
-       
+        bool convertToF = true;
 
         public MainPage()
         {
@@ -12,22 +12,53 @@
 
         private void ConvertButton_Clicked(object sender, EventArgs e)
         {
-            if (double.TryParse(CelsiusEntry.Text, out double celsius))
+            if( convertToF == true )
             {
-                double fahrenheit = UnitConverters.CelsiusToFahrenheit(celsius);
-                FahrenheitEntry.Text = $"{fahrenheit:F2}";
-            }
-            else
+                if (double.TryParse(CelsiusEntry.Text, out double celsius))
+                {
+                    ErrorTextField.Text = ""; // clear the error message
+
+                    double fahrenheit = UnitConverters.CelsiusToFahrenheit(celsius);
+                    FahrenheitEntry.Text = $"{fahrenheit:F2}";
+                }
+                else
+                {
+                    // Handle invalid input (e.g., non-numeric values).
+                    ErrorTextField.Text = "Invalid input. Please enter a valid Celsius temperature.";
+                }
+            } else
             {
-                // Handle invalid input (e.g., non-numeric values).
-                ErrorTextField.Text = "Invalid input. Please enter a valid Celsius temperature.";
+                if (double.TryParse(FahrenheitEntry.Text, out double fahrenheit))
+                {
+                    ErrorTextField.Text = ""; // clear the error message
+
+                    double celsius = UnitConverters.FahrenheitToCelsius(fahrenheit);
+
+                    CelsiusEntry.Text = $"{celsius:F2}";
+                }
+                else
+                {
+                    // Handle invalid input (e.g., non-numeric values).
+                    ErrorTextField.Text = "Invalid input. Please enter a valid ftemperature.";
+                }
             }
         }
 
         private void SwitchDirectionClicked(object sender, EventArgs e)
         {
-            count++;
-            ErrorTextField.Text = $"Switch direction button pressed {count} times";
+            convertToF = !convertToF;
+
+            if (convertToF == true)
+            {
+                ConversionToggle.Text = ">";
+                ErrorTextField.Text = "Switch direction to F";
+            }
+            else
+            {
+                ConversionToggle.Text = "<";
+                ErrorTextField.Text = "Switch direction to C";
+
+            }
         }
     }
 
